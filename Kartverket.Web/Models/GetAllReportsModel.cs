6883b6dbf5e50ec463b2fc7d90e54984f616dc7f
Bruker <emvisic@gmail.com>
2025-09-23 10:48:56 +0200

    WOOP WOOP

--- Kartverket.Web/Models/GetAllReportsModel.cs
+++ Kartverket.Web/Models/GetAllReportsModel.cs
@@ -1,9 +1,11 @@
 ﻿using Kartverket.Web.Database.Tables;
+// Modell for sending data to view
 
 namespace Kartverket.Web.Models
 {
     public class GetAllReportsModel
     {
+        // List for reports
         public List<MakeReportList> Reports { get; set; } = [];
         public class MakeReportList
         {
