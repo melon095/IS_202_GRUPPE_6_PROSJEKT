3b273e29fa2c3b747db59459a2706f5c3c025dbf
Joachim Flottorp <joachim@flottorp.org>
2025-10-07 11:54:10 +0200

    Fjern aspire og legg till docker compose

--- Kartverket.Web/Program.cs
+++ Kartverket.Web/Program.cs
@@ -6,9 +6,6 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.AddServiceDefaults();
-
-// Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services
     .AddSingleton<DummyMapService, DummyMapService>()
@@ -43,14 +40,16 @@ builder.Services.AddSession(options =>
 
 var app = builder.Build();
 
-app.MapDefaultEndpoints();
-
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+}
+
+if (Environment.GetEnvironmentVariable("DOTNET_RUNNING_IN_CONTAINER") == "true")
+{
+    var db = app.Services.CreateScope().ServiceProvider.GetRequiredService<DatabaseContext>();
+    db.Database.Migrate();
 }
 
 app.UseHttpsRedirection();
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Kartverket
+
+Denne repoen inneholder Gruppe 6 sin Kartverk oppgave. Den er ikke ferdig!
+
+Du kan starte prosjektet ved å kjøre følgende kommandoer:
+
+```bash
+docker compose up
+```
