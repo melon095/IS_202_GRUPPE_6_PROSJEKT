88c6b42bd27a6f315bf38a6e7e97a88dc9d8973f
Bruker <emvisic@gmail.com>
2025-10-07 13:16:22 +0200

    Beggynelse på ny rapport side

---
+++ Kartverket.Web/Models/GetAllReportsModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Kartverket.Web.Models
+{
+    public class GetAllReportsModel
+    {
+        // List of reports
+        public List<MakeReportList> Reports { get; set; } = []
+        public class MakeReportList
+        {
+            public Guid Id { get; set; }
+            public string User { get; set; }
+            public string Title { get; set; }
+            public DateTime CreatedAt { get; set; }
+        }
+    }
+}
