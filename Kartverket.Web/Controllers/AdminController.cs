ba2e3227a3ed69853dde4d3ece20f66cde8d4f4d
Bruker <emvisic@gmail.com>
2025-10-08 11:53:28 +0200

    Report in depth

--- Kartverket.Web/Controllers/AdminController.cs
+++ Kartverket.Web/Controllers/AdminController.cs
@@ -1,7 +1,9 @@
 ﻿using Kartverket.Web.Database;
 using Kartverket.Web.Models.Admin;
 using Kartverket.Web.Models.Admin.Request;
+using Kartverket.Web.Models.Report.Response;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 namespace Kartverket.Web.Controllers;
 
 [Controller]
@@ -15,10 +17,38 @@ public class AdminController: Controller
         _logger = logger;
         _dbContext = ctx;
     }
+
+    [HttpGet("/Admin/ReportInDepth/{id:guid}")]
+    public IActionResult ReportInDepth(Guid id)
+    {
+        var Model = new InDepthReportModel();
+        var report = _dbContext.Reports
+            .Include(r => r.User)
+            .Include(r => r.MapPoints)
+            .FirstOrDefault(r => r.Id == id);
+        foreach(var point in report.MapPoints)
+        {
+            Model.Points.Add(new InDepthReportModel.Point
+            {
+                Lat = point.Latitude,
+                Lng = point.Longitude,
+                Elevation = point.AMSL
+            });
+        }
+        Model.Id = report.Id;
+        Model.Title = report.Title;
+        Model.Description = report.Description;
+        Model.CreatedAt = report.CreatedAt;
+        if(report == null)
+        {
+            return NotFound();
+        }
+        return View(Model);
+    }
     
     public IActionResult Index()
     {
-        var reports =  _dbContext.Reports;
+        var reports =  _dbContext.Reports.ToList();
         var Model = new GetAllReportsModel();
         foreach (var report in reports)
         {
---
+++ Kartverket.Web/Models/Report/Response/InDepthReportModel.cs
@@ -0,0 +1,20 @@
+﻿namespace Kartverket.Web.Models.Report.Response
+{
+    public class InDepthReportModel
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public List<Point> Points { get; set; } = [];
+
+        public struct Point
+        {
+            public double Lat { get; set; }
+            public double Lng { get; set; }
+            public double Elevation { get; set; }
+        }
+    }
+}
