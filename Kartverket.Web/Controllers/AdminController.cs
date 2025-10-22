cd22ba5dbf8d81e3630c6c4a1fd8ef04d5c0fce4
Emil-S-V <kjakkemil@gmail.com>
2025-10-22 14:05:19 +0200

    Kan vi adde rappor TV3 til main? jeg ønsker please (#35)

--- Kartverket.Web/Controllers/AdminController.cs
+++ Kartverket.Web/Controllers/AdminController.cs
@@ -1,27 +1,141 @@
-﻿using Kartverket.Web.AuthPolicy;
 using Kartverket.Web.Database;
 using Kartverket.Web.Models.Admin.Request;
-using Microsoft.AspNetCore.Authorization;
+using Kartverket.Web.Models.Admin;
+using Kartverket.Web.Models.Report.Response;
 using Microsoft.AspNetCore.Mvc;
-
+using Microsoft.EntityFrameworkCore;
 namespace Kartverket.Web.Controllers;
 
 [Controller]
-public class AdminController: Controller
+public class AdminController : Controller
 {
     private readonly ILogger<AdminController> _logger;
     private readonly DatabaseContext _dbContext;
-    
+    private const int ReportPerPage = 10;
+
     public AdminController(ILogger<AdminController> logger, DatabaseContext ctx)
     {
         _logger = logger;
         _dbContext = ctx;
     }
-    
-    [Authorize(Policy = RoleValue.AtLeastKartverket)]
-    public IActionResult Index()
+
+    [HttpGet("/Admin/ReportInDepth/{id:guid}")]
+    public IActionResult ReportInDepth(Guid id, [FromQuery] Guid? objectID)
     {
-        return View();
+        var report = _dbContext.Reports
+            .Include(r => r.User)
+            .Include(r => r.MapObjects)
+            .ThenInclude(mo => mo.MapPoints)
+            .FirstOrDefault(r => r.Id == id);
+        if (report == null)
+        {
+            return  View("NoObjectsErr");
+        }
+
+        var selectedObject = report.MapObjects
+            .Where(x => x.Id == objectID)
+            .SingleOrDefault();
+
+        var Model = new InDepthReportModel
+        {
+            Id = report.Id,
+            Title = report.Title,
+            Description = report.Description,
+            CreatedAt = report.CreatedAt,
+        };
+
+        foreach (var objects in report.MapObjects)
+        {
+            var objectData = new InDepthReportModel.ObjectDataModel()
+            {
+                Id = objects.Id,
+                Title = objects.Title,
+                Description = objects.Description
+            };
+            foreach (var point in objects.MapPoints)
+            {
+                objectData.Points.Add(new InDepthReportModel.ObjectDataModel.Point
+                {
+                    Lat = point.Latitude,
+                    Lng = point.Longitude,
+                    Elevation = point.AMSL
+                });
+            }
+
+            if (objectData.Points.Count > 0)
+            {
+                var GetCentroid = new InDepthReportModel.ObjectDataModel.Point
+                {
+                    Lat = objectData.Points.Average(p => p.Lat),
+                    Lng = objectData.Points.Average(p => p.Lng),
+                    Elevation = 0
+                };
+
+                objectData.CentroidPoint = GetCentroid;
+
+                Model.Objects.Add(objectData);
+
+                if (selectedObject != null && objects.Id == selectedObject.Id)
+                {
+                    Model.SelectedObject = objectData;
+                }
+            }
+
+        }
+
+        return View(Model);
+    }
+
+    [HttpGet]
+    public IActionResult Index([FromQuery] int page = 1, [FromQuery] DateOnly? sortDate = null, [FromQuery] string sortOrder = "desc")
+    {
+        sortDate ??= DateOnly.FromDateTime(DateTime.Now);
+        // Pagnering når det er for mange rapporter
+        // Sorterer etter dato
+        var reportQuery = _dbContext.Reports.AsQueryable();
+
+        reportQuery = reportQuery
+            .Where(r => DateOnly.FromDateTime(r.CreatedAt) <= sortDate.Value);
+
+        var totalReports = reportQuery.Count();
+        var totalpages = (int)Math.Ceiling(totalReports / (double)ReportPerPage);
+
+        reportQuery = sortOrder.ToLower() switch
+        {
+            "asc" => reportQuery.OrderBy(r => r.CreatedAt),
+            _ => reportQuery.OrderByDescending(r => r.CreatedAt)
+        };
+
+        var reports = reportQuery
+            .Skip((page - 1) * ReportPerPage)
+            .Take(ReportPerPage)
+            .Include(r => r.MapObjects)
+            .ToList();
+
+        var Model = new GetAllReportsModel()
+        {
+            CurrentPage = page,
+            TotalPages = totalpages,
+            SortDate = sortDate.Value
+        };
+
+
+        foreach (var report in reports)
+        {
+            Model.Reports.Add(new GetAllReportsModel.MakeReportList
+            {
+                Id = report.Id,
+                Title = report.Title,
+                CreatedAt = report.CreatedAt,
+                TotalObjects = report.MapObjects.Count
+            });
+        }
+
+
+
+        ViewBag.SortOrder = sortOrder.ToLower();
+
+        return View(Model);
     }
 
     [HttpGet]
