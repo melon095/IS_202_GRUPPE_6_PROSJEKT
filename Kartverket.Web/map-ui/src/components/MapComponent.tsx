d0bc663e437ec6b9909ef917d31fc5228073b832
Joachim Flottorp <joachim@flottorp.org>
2025-10-27 12:39:46 +0100

    Nei gps funker tenker jeg

--- Kartverket.Web/map-ui/src/components/MapComponent.tsx
+++ Kartverket.Web/map-ui/src/components/MapComponent.tsx
@@ -1,34 +1,36 @@
-import { LatLngTuple } from "leaflet";
+import {LatLngTuple} from "leaflet";
 import "leaflet/dist/leaflet.css";
 import React from "react";
-import { MapContainer, TileLayer, TileLayerProps } from "react-leaflet";
+import {MapContainer, TileLayer, TileLayerProps} from "react-leaflet";
 
 import "../css/MapComponent.css";
 import "../css/zoom-control.css";
-import { MapClickHandler } from "./MapClickHandler";
-import { ObjectMarkers } from "./ObjectMarkers";
-import { ServerMarkers } from "./ServerMarkers";
+import {MapClickHandler} from "./MapClickHandler";
+import {ObjectMarkers} from "./ObjectMarkers";
+import {ServerMarkers} from "./ServerMarkers";
+import {GPSMarker} from "./GPSMarker.tsx";
 
 const mapCenter = [58.1465456, 7.9911451] satisfies LatLngTuple;
 
 const tileProps = {
-	attribution: `&copy; <a href="http://www.kartverket.no/">Kartverket</a>`,
-	url: `https://cache.kartverket.no/v1/wmts/1.0.0/topo/default/webmercator/{z}/{y}/{x}.png`,
+    attribution: `&copy; <a href="https://www.kartverket.no/">Kartverket</a>`,
+    url: `https://cache.kartverket.no/v1/wmts/1.0.0/topo/default/webmercator/{z}/{y}/{x}.png`,
 } satisfies TileLayerProps;
 
 interface MapComponentProps {
-	children?: React.ReactNode;
+    children?: React.ReactNode;
 }
 
-export const MapComponent = ({ children }: MapComponentProps) => {
-	return (
-		<MapContainer center={mapCenter} zoom={13} style={{ height: "100vh", width: "100vw" }} zoomControl={false}>
-			<TileLayer {...tileProps} />
-			<MapClickHandler />
-			<ObjectMarkers />
-			<ServerMarkers />
+export const MapComponent = ({children}: MapComponentProps) => {
+    return (
+        <MapContainer center={mapCenter} zoom={13} style={{height: "100vh", width: "100vw"}} zoomControl={false}>
+            <TileLayer {...tileProps} />
+            <MapClickHandler/>
+            <ObjectMarkers/>
+            <ServerMarkers/>
+            <GPSMarker/>
 
-			{children}
-		</MapContainer>
-	);
+            {children}
+        </MapContainer>
+    );
 };
---
+++ Kartverket.Web/map-ui/src/contexts/LocationProvider.tsx
@@ -0,0 +1,30 @@
+﻿import {createContext, ReactNode, useContext} from 'react';
+import {useGeolocated} from 'react-geolocated';
+
+const LocationContext = createContext<ReturnType<typeof useGeolocated> | null>(null);
+
+export interface LocationProviderProps {
+    children: ReactNode;
+}
+
+export function LocationProvider({children}: LocationProviderProps) {
+    const geolocated = useGeolocated({
+        positionOptions: {
+            enableHighAccuracy: false,
+        },
+    });
+
+    return (
+        <LocationContext.Provider value={geolocated}>
+            {children}
+        </LocationContext.Provider>
+    );
+}
+
+export function useLocation() {
+    const context = useContext(LocationContext);
+    if (!context) {
+        throw new Error('useLocation must be used within LocationProvider');
+    }
+    return context;
+}
