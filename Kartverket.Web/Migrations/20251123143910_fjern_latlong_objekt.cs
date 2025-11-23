49f6f30faf204330150bb9c735d79a21251621a9
Joachim Flottorp <joachim@flottorp.org>
2025-11-23 17:20:32 +0100

    Fjerne latlong fra databse (#68)

---
+++ Kartverket.Web/Migrations/20251123143910_fjern_latlong_objekt.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Kartverket.Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class fjern_latlong_objekt : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Latitude",
+                table: "HindranceObjects");
+
+            migrationBuilder.DropColumn(
+                name: "Longitude",
+                table: "HindranceObjects");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "Latitude",
+                table: "HindranceObjects",
+                type: "double",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Longitude",
+                table: "HindranceObjects",
+                type: "double",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+    }
+}
